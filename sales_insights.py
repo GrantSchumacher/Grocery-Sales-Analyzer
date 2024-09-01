4db5f22ee4e9980ec2d7dd9861f73f9cb6f69e83
Grant Schumacher <grantschumacher@Grant-MacBook-Pro.local>
2024-09-01 14:18:10 -0600

    Added main menu option to select an insight to generate. Added error handling for unselected options with a prompt to try again. Fixed bug where an incorrect file name would redirect to the main menu; now prompts for another file.

--- sales_insights.py
+++ sales_insights.py
@@ -6,16 +6,44 @@ def load_file():
     file_path = input("Enter the path of the excel file: \n").strip()
 
     try:
-        data = pd.read_excel(file_path)
+        data = pd.read_excel(file_path, header = 1)
         print("data loaded succesfully")
         return data
     except Exception as e:
-        print("error loading file. Please try again.")
-        print(e)
+        print("Error loading file. Please try again.")
+        #print(e)
+        load_file()
         return None
     
 
-load_file()
+def main_menu():
+    print("""
+    Please select an option
+          1. Top Performing Store
+          2. Exit
+    """)
+    selected_menu_option = input()
+    return selected_menu_option
+
+def run_application():
+    while True:
+        selected_menu_option = main_menu()
+        if selected_menu_option == '1':
+            print("you've selected option 1")
+        elif selected_menu_option == '2':
+            print("Exiting Application")
+            break
+        else:
+            print("Invalid option. Please try again")
+
+    
+df = load_file()
+run_application()
+
+
+
+
+
 
 
     
