b4004ce20c96d4c6abb6503e6c147d61de1fd023
Grant Schumacher <grantschumacher@Grant-MacBook-Pro.local>
2024-09-01 13:42:16 -0600

    Add load_file function to prompt for an Excel file path and load it using pandas' read_excel. Includes error handling to catch and print exceptions if loading fails.

--- sales_insights.py
+++ sales_insights.py
@@ -1,5 +1,21 @@
+import pandas as pd
 
-def main_menu():
+print("--Sales Data Analyzer--\n")
+
+def load_file():
+    file_path = input("Enter the path of the excel file: \n").strip()
+
+    try:
+        data = pd.read_excel(file_path)
+        print("data loaded succesfully")
+        return data
+    except Exception as e:
+        print("error loading file. Please try again.")
+        print(e)
+        return None
+    
+
+load_file()
 
 
     
